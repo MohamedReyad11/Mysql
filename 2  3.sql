419a3d19770eab87a50da8f177400c99a31efabc
MohamedReyad11 <100624587+MohamedReyad11@users.noreply.github.com>
2022-03-02 20:38:44 +0200

    Add files via upload
    TASK 2

---
+++ 2  3.sql
@@ -0,0 +1,11 @@
+CREATE TABLE parent (
+    id INT NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=INNODB;
+CREATE TABLE child (
+    id INT,
+    parent_id INT,
+    INDEX par_ind (parent_id),
+    FOREIGN KEY (parent_id)
+        REFERENCES parent(id)
+) ENGINE=INNODB;
